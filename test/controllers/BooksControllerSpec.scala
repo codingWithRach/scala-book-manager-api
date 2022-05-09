b75b0338b14001f46a5d0bbd28f8767599fd7cf3
Rachel Miles <rachmiles@gmail.com>
2022-05-09 18:02:13 +0100

    Implement test for deletion of book

--- test/controllers/BooksControllerSpec.scala
+++ test/controllers/BooksControllerSpec.scala
@@ -81,4 +81,19 @@ class BooksControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injectin
       contentType(book) mustBe Some("application/json")
     }
   }
+
+  "BooksController DELETE bookById" should {
+
+    "return 200 OK for deleting a single book" in {
+
+      // Here we utilise Mockito for stubbing the request to deleteBook
+      when(mockDataService.deleteBook(1)) thenReturn sampleBook
+
+      val controller = new BooksController(stubControllerComponents(), mockDataService)
+      val book = controller.deleteBook(1).apply(FakeRequest(DELETE, "/books/1"))
+
+      status(book) mustBe OK
+      contentType(book) mustBe Some("application/json")
+    }
+  }
 }
